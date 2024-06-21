d51fe2869d1e6c6fd6230ff4a17ce9874d978d8c
Ben Manning <bmanning@me.com>
2024-06-21 12:20:19 -0400

    create fruit schema

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
+const Fruit = require('./models/fruit.js');
+
 // GET /
 app.get('/', async (req, res) => {
   res.render('index.ejs');
