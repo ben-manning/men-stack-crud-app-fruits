be2b73d98c1b5d85252c4cdc5977c90c4ea78b96
Ben Manning <bmanning@me.com>
2025-02-14 12:43:18 -0500

    configure the app for mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected on MongoDB ${mongoose.connection.name}`);
+});
+
+
 
 // GET /
 app.get('/', async (req, res) => {
