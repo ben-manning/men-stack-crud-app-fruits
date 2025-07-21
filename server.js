d3ccd7e10c4243b140d18bac6fc141d320521e15
Ben Manning <bmanning@me.com>
2025-07-21 12:05:21 -0700

    create fruit edit template

--- server.js
+++ server.js
@@ -64,6 +64,12 @@ app.delete('/fruits/:fruitId', async (req, res) => {
   res.redirect('/fruits');
 });
 
+app.get('/fruits/:fruitId/edit', async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  console.log(foundFruit);
+  res.render('fruits/edit.ejs', { fruit: foundFruit });
+});
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
